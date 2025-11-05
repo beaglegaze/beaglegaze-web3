c4d3cfe8b4380b8505a51921fbccc27b9c81c179
Steffen Börner <steffen.boerner@exxeta.com>
2025-11-05 08:56:32 +0100

    Pass in context argument in build script

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # copy contract files to eth-dev-node directory
+# Accept context as first argument, default to 'contract' if not provided
+CONTEXT=${1}
 cp -r contract/* eth-dev-node/contracts/
 
 # build hardhat-testnet docker image
-docker build -t hardhat-testnet eth-dev-node/.
+docker build -t hardhat-testnet $CONTEXT/eth-dev-node/.
 
