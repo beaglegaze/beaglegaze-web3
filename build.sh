a46e8a08cdfd4dc471c68833e33f1539358189d7
steffenboe <steffenb1991.8@gmail.com>
2025-11-11 20:28:29 +0000

    Add context

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # copy contract files to eth-dev-node directory
 # Accept context as first argument, default to 'contract' if not provided
 CONTEXT=${1}
-cp -r contract/* eth-dev-node/contracts/
+cp -r $CONTEXT/contract/* $CONTEXT/eth-dev-node/contracts/
 
 # build hardhat-testnet docker image
 docker build -t hardhat-testnet $CONTEXT/eth-dev-node/.
